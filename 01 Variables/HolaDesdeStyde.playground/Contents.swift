f36f3421e6c0ccefcd101aa1a08f44bf04384c08
Jorge Jimenez <xadrijo@hotmail.com>
2016-04-25 21:54:19 -0430

    First commit

---
+++ 01 Variables/HolaDesdeStyde.playground/Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+
+/*Esto es un comentario de multiples líneas
+ 
+ y me sirve de test para ser mostrado en
+ 
+ Styde.net. Saludos*/
+
+import UIKit
+
+var str = "Hello, playground"
+
+var holaStyde: String = "Hola desde Styde"
+
+var saludo = "Hola a secas"
+
+//var myVariable
+
+var saludo = 3
+
