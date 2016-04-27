c883e489995cbbf11bec850742905165885a1d7d
Jorge Jimenez <xadrijo@hotmail.com>
2016-04-27 14:37:05 -0430

    Lesson 1.2 - Video 04

---
+++ 02 Constantes/Constantes.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let saludo: String = "Hola desde Styde"
+let numero: Int = 7
+let decimal: Double = 5.0
+
+let mySaludo = "Hola desde Styde de manera implicita"
+
+
+//numero = 3
