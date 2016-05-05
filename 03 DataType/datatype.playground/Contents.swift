43dea3cb8326c2be1947370461e96ad3d50829d1
Jorge Jimenez <xadrijo@hotmail.com>
2016-05-05 14:01:15 -0430

    Lesson 1.3 - Video 05

---
+++ 03 DataType/datatype.playground/Contents.swift
@@ -0,0 +1,38 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// Enteros
+var num1 = 5
+var num2 = 3
+
+var suma = num1 + num2
+var resta = num1 - num2
+var mult = num1 * num2
+var div = num1 / num2
+
+var modulus = num1 % num2
+
+// Double
+var num3 = 5.0
+var num4 = 3.0
+
+var div1 = num3 / num4
+
+var sumaMixta = Double(num1) + num3
+
+// String
+var saludo = "Hola, "
+var nombre = "Styde"
+
+var concatenacion = saludo + nombre
+var city = " desde Merida"
+
+var concatenacion1 = saludo + nombre + city
+
+// Bool
+var verdadero = true
+var falso = false
+
+//var verdadero1: Bool = 1
+
