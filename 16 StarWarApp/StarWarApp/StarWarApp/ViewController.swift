80d6d01bcf0e12c772affb3b8a97afdedabd904e
Jorge Jimenez <xadrijo@hotmail.com>
2016-08-19 09:04:11 -0400

    Lesson 1.16 - Video 18

--- 16 StarWarApp/StarWarApp/StarWarApp/ViewController.swift
+++ 16 StarWarApp/StarWarApp/StarWarApp/ViewController.swift
@@ -7,19 +7,84 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var hairColorLabel: UILabel!
+    @IBOutlet weak var eyeColorLabel: UILabel!
+    @IBOutlet weak var genderLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let swEndpoint = "http://swapi.co/api/people/2/"
+        
+        guard let url = NSURL(string: swEndpoint) else {
+            print("No se pudo crear la URL")
+            return
+        }
+        
+        let urlRequest = NSURLRequest(URL: url)
+        let session = NSURLSession.sharedSession()
+        
+        let task = session.dataTaskWithRequest(urlRequest) { (data: NSData?, response: NSURLResponse?, error: NSError?) in
+            
+            guard let responseData = data else {
+                print("Data no recibida")
+                return
+            }
+            
+            do {
+                guard let people = try NSJSONSerialization.JSONObjectWithData(responseData, options: []) as? [String : AnyObject] else {
+                    print("Error tratando de convertir data a JSON")
+                    return
+                }
+                
+                print(people["name"])
+                print(people["height"])
+                print(people["hair_color"])
+                print(people["eye_color"])
+                print(people["gender"])
+                
+                let peopleName = people["name"] as? String
+                let peopleHeight = people["height"] as? String
+                let peopleHairColor = people["hair_color"] as? String
+                let peopleEyeColor = people["eye_color"] as? String
+                let peopleGender = people["gender"] as? String
+                
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    self.updateUI(peopleName!, height: peopleHeight!, hairColor: peopleHairColor!, eyeColor: peopleEyeColor!, gender: peopleGender!)
+                })
+                
+                
+                
+            } catch {
+                print("Error tratando de convertir data a JSON")
+            }
+        }
+        
+        task.resume()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func updateUI(name: String, height: String, hairColor: String, eyeColor: String, gender: String) {
+        nameLabel.text = name
+        heightLabel.text = height
+        hairColorLabel.text = hairColor
+        eyeColorLabel.text = eyeColor
+        genderLabel.text = gender
     }
-
 
 }
+
+
+
+
+
+
+
+
 
